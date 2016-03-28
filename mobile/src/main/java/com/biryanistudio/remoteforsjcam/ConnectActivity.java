76a92bb66f10deea9341088eca409ebd10e7e4ae
Aakaash Jois <aakaashjois@gmail.com>
2016-03-28 23:27:12 +0530

    Merge branch 'master' into pr/1

--- mobile/src/main/java/com/biryanistudio/remoteforsjcam/ConnectActivity.java
+++ mobile/src/main/java/com/biryanistudio/remoteforsjcam/ConnectActivity.java
@@ -3,17 +3,10 @@ package com.biryanistudio.remoteforsjcam;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
-import android.widget.VideoView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import org.videolan.libvlc.media.VideoView;
+
 
 public class ConnectActivity extends AppCompatActivity {
 
@@ -29,10 +22,5 @@ public class ConnectActivity extends AppCompatActivity {
     {
         String url = "rtsp://192.168.1.254/sjcam.mov";
         videoView.setVideoURI(Uri.parse(url));
-        videoView.requestFocus();
         videoView.start();
-        int height = videoView.getHeight();
-        int width = videoView.getWidth();
-        Log.i("SJCAM","Height = " + Integer.toString(height));
-        Log.i("SJCAM","Width = " + Integer.toString(width));
     }}
