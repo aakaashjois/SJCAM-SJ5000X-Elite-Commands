5672f743bf767255693077afeec6ad60e622ab02
Aakaash Jois <aakaashjois@gmail.com>
2016-05-28 11:45:25 +0530

    Updated the libraries to the latest version. Changed the layout to Constraint Layout.

--- mobile/src/main/java/com/biryanistudio/remoteforsjcam/ConnectActivity.java
+++ mobile/src/main/java/com/biryanistudio/remoteforsjcam/ConnectActivity.java
@@ -1,8 +1,8 @@
 package com.biryanistudio.remoteforsjcam;
 
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import org.videolan.libvlc.media.VideoView;
@@ -10,17 +10,19 @@ import org.videolan.libvlc.media.VideoView;
 
 public class ConnectActivity extends AppCompatActivity {
 
-    VideoView videoView;
+    private VideoView videoView;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
         videoView = (VideoView) findViewById(R.id.video);
     }
 
-    public void startVideo(View view)
+    public void startVideo(final View view)
     {
-        String url = "rtsp://192.168.1.254/sjcam.mov";
+        final String url = "rtsp://192.168.1.254/sjcam.mov";
         videoView.setVideoURI(Uri.parse(url));
         videoView.start();
     }}
+
+
